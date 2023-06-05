0f600b04a9d5d88556689886d85148a785cf5688
Lev Stambler <levstamb@gmail.com>
2023-06-05 11:53:21 +0100

    debug start

--- IncrRuntimeAttr.lean
+++ IncrRuntimeAttr.lean
@@ -1,6 +1,5 @@
 import Lean
 import Mathlib.Lean.Meta
-
 open Lean Meta Elab
 open Nat
 
@@ -32,23 +31,57 @@ def mkRuntimePropImpl (ind : Name) (rel : Name) (relStx : Syntax) : MetaM Unit :
  
   -- TODO: get rid of return type?
   -- TODO: I dont think that this is right
-  let runtime_type := Expr.lam `args defVal.type (.app defVal.value (.bvar 0)) .default
+  -- #print defVal.type
+  -- TODO: type reinforcement
+  let runtime_type := match defVal.type with
+  | Expr.forallE a inpType resType _ => Expr.forallE a inpType 
+      (mkAppN (.const ``Prod.mk [Level.succ .zero, Level.succ .zero])
+             #[Expr.sort (Level.succ .zero), Expr.sort (Level.succ .zero),  .const ``Nat [], resType]) .default
+  | _ => Expr.const ``Nat [] -- TODO: put in error throw
+  -- TODO: put in error throw
+  -- | _ => throwError "The input type must be a function with a →"
 
   -- := Expr.lam `args defVal.type (match defVal.value with
   -- ) .default
   -- TODO:?
   let rec runtime_value : Expr → Expr 
-    | Expr.app f args => mkAppN (.const ``Nat.add []) #[.app (.const ``Nat.succ []) (.const ``Nat.zero []),
-        runtime_value f] 
+    | Expr.app f args => 
+      mkAppN (.const ``Prod.mk []) #[
+        -- Runtime
+        mkAppN (.const ``Nat.add []) #[
+          -- Add the runtime accumulated in `f`
+          mkAppN (.const ``Nat.add []) #[
+            .app (
+              mkAppN (.const
+                ``Nat.add []) #[
+                  .app (.const ``Nat.succ []) (.const ``Nat.zero []),
+                  runtime_value f -- TODO: change to add with **projection**
+                ] 
+            ) (runtime_value f),
+            (runtime_value args)],
+          -- Add the runtime accumalated in `args`
+          runtime_value args
+        ],
+        -- Value
+        Expr.app f args
+      ]
+      
+    | Expr.lam argName argType body _ => 
+      .lam argName argType (runtime_value body) .default 
+    | .const a ℓ => --.const ``Nat.zero []
+         mkAppN (.const ``Prod.mk []) #[
+           .const ``Nat.zero [],
+           .const a ℓ
+         ]
     | _ => .const ``Nat.zero []
 
   -- Add decleration of Runtime Function
   addDecl $ .defnDecl {
-    value  := defVal.value -- want this value to be correct
+    value  := runtime_value defVal.value -- want this value to be correct
     hints  := defVal.hints
     safety := defVal.safety -- not quite sure about how to do saftey but should be exact same
     levelParams := defVal.levelParams
-    type := runtime_type -- Want this type to be correct
+    type := .forallE `a (.const ``Nat []) (mkAppN (.const ``Prod.mk [Level.succ .zero, Level.succ .zero]) #[Expr.sort (Level.succ .zero), Expr.sort (Level.succ .zero), Expr.const ``Nat [], Expr.const ``Bool []]) .default --runtime_type -- Want this type to be correct
     name := rel
   }
   -- addDeclarationRanges rel {
--- bigO.lean
+++ bigO.lean
@@ -1,8 +1,10 @@
 import Lean
 import Mathlib
-import IncrRuntimeAttr
+-- import IncrRuntimeAttr
+open Lean Meta Elab PrettyPrinter Delaborator
+open Nat
+open List
 
-open Lean
 namespace BigO
 
 -- Ok, so we may want to use something "monoid esque" but also maybe not
@@ -12,8 +14,11 @@ namespace BigO
 --   f : α → β
 --   runtime: Nat
 
+#check Prod.mk Nat Nat
+
 inductive RuntimeFun where
   | func {α β : Type } (runtime : Nat) (g : α → β) : RuntimeFun  
+
 
 -- instance : Monad RuntimeFun where
 --   pure f  := RuntimeFun.func 0  f
@@ -25,14 +30,78 @@ inductive RuntimeFun where
 --   coe F := match F with
 --   | RuntimeFun.func _ g => g
 -- This is what I'd like
-@[incr_runtime] def lin_ex_2 : Nat → Bool
-  | 0 => true
-  | n + 1 => lin_ex_2 n
+-- def lin_ex_2 : Nat → Bool
+--   | 0 => false
+--   | n + 1 => lin_ex_2 n
+-- @[incr_runtime]
+-- def lin_ex_2 (n : Nat) : Bool := false
 
-#eval 10 + 1
-#check lin_ex_2runtime
+def runtime_value : Expr → Expr 
+    | Expr.app f args => 
+        mkAppN (.const ``Prod.mk .nil) #[
+          .const ``Nat List.nil,
+          .const ``Bool List.nil,
+          -- Runtime
+          mkAppN (.const ``Nat.add List.nil) #[
+            -- Add the runtime accumulated in `f`
+            mkAppN (.const ``Nat.add .nil) #[
+              .app (
+                mkAppN (.const
+                  ``Nat.add .nil) #[
+                    .app (.const ``Nat.succ List.nil) (.const ``Nat.zero List.nil),
+                    runtime_value f -- TODO: change to add with **projection**
+                  ] 
+              ) (runtime_value f),
+              (runtime_value args)],
+            -- Add the runtime accumalated in `args`
+            runtime_value args
+          ],
+          -- Value
+          Expr.app f args
+        ]
+      
+    | Expr.lam argName argType body _ => 
+      .lam argName argType (runtime_value body) .default 
+    | .const `a ℓ => --.const ``Nat.zero []
+         mkAppN (.const ``Prod.mk .nil) #[
+            .const ``Nat List.nil,
+            .const ``Bool .nil,
+           .const ``Nat.zero .nil,
+          .bvar 0--  .const `a ℓ
+          --  .const `Bool.true .nil
+         ]
+    | _ => mkNatLit 69
+  
+#check runtime_value
+def lin_ex_2 (n : Nat) : Bool := false
+-- Hmmm... I guess we can j write a macro
+#eval Expr.const ``lin_ex_2 .nil
 
-#eval lin_ex_2 10
+#eval (runtime_value (.lam `a  (.const `Nat .nil) (.const `a .nil) .default))
+
+def ee : Expr := 
+  (runtime_value (.lam `a  (.const `Nat .nil) (.const `a .nil) .default))
+-- #check ee
+
+@[delab app.foo]
+def dee : Delab := (delab ee)
+
+#check (foo)
+#eval (foo) false
+-- #eval ee false
+
+
+
+-- #check let dd := mkPatternWithRef (.const ``Nat.zero []) `(fun n : Nat => false); dd
+-- #check Lean.LocalDecl.toExpr 
+
+-- #check dd -->>=  fun x => x
+-- #eval 10 + 1
+-- #check lin_ex_2Runtime
+-- #print lin_ex_2Runtime
+-- #eval lin_ex_2Runtime 10
+
+-- #eval lin_ex_2 10
 
 syntax term "^^ " term : term
 
